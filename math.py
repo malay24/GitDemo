7623da889c9a59cfed0b73d0067d9b9de07e8cc3
Malay <malay_sourav@persistent.com>
2021-01-25 14:59:20 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+d Perform addition
+def add(x,y):
+    return x+y
+# Perform subtraction
+def subtract(x,y):
+    if x<y:
+	  return Error
+    return x-y
+# Perform multiplication
+def multiply(x,y):
+    return x*y
+    
+# Perform division
+def divide(x,y):
+    if y==0:
+	 return ERROR
+	return x/y
+
+def square(x,y):
+   return x*x
